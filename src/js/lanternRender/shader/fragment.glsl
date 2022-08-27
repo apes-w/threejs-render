f027888bdcb596fda39fc187c026c280b19590e8
wind-hao-laptop <windapes@163.com>
2022-08-27 15:29:50 +0800

    根据物体的高度实现明暗的渐变

--- src/js/lanternRender/shader/fragment.glsl
+++ src/js/lanternRender/shader/fragment.glsl
@@ -9,5 +9,14 @@ void main() {
   vec3 redColor = vec3(1.0, 0.0, 0.0);
   vec3 yellowColor = vec3(1.0, 1.0, 0.3);
   vec3 mixColor = mix(yellowColor, redColor, (gPosition.y - 0.2) / 3.0);
-  gl_FragColor = vec4(mixColor, 1.0);
+
+  /*
+    判断物体的面的朝向
+    这个值本身是一个bool，可以通过 true 和 false 来知道当前面试正面还是反面
+  */
+  if (gl_FrontFacing) {
+    gl_FragColor = vec4(mixColor.xyz - (vPosition.y - 40.0) / 200.0, 1);
+  } else {
+    gl_FragColor = vec4(mixColor, 1.0);
+  }
 }
