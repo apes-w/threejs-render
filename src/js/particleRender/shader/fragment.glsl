cde3206553015c0d4a6e0b1741603aba3229465e
wind-hao-laptop <windapes@163.com>
2022-09-07 15:57:23 +0800

    添加渲染效果

--- src/js/particleRender/shader/fragment.glsl
+++ src/js/particleRender/shader/fragment.glsl
@@ -2,6 +2,12 @@ precision mediump float;
 
 #define PI 3.14159265359
 
+varying vec2 vUv;
+
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  // 使用uv向量，实际效果为黑色方块
+  // gl_FragColor = vec4(vUv, 0.0, 1.0);
+
+  // gl_PointCoord 点渲染模式对应点像素坐标
+  // gl_FragColor = vec4(vec2(gl_PointCoord), 0.0, 1.0);
 }
--- src/js/particleRender/shader/vertex.glsl
+++ src/js/particleRender/shader/vertex.glsl
@@ -4,9 +4,14 @@ precision mediump float;
 // mediump 中精度 -2^10 - 2^10
 // highp 高精度 -2^16 - 2^16
 
+varying vec2 vUv;
+
 void main() {
   vec4 resultPosition = modelMatrix * vec4(position, 1.0);
   gl_Position = projectionMatrix * viewMatrix * resultPosition;
 
   gl_PointSize = 30.0;
+
+  vUv = uv;
+
 }
