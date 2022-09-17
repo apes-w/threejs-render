4c523135d456c5a537821c4832b03e3855b9f525
wind-hao-laptop <windapes@163.com>
2022-09-17 15:54:28 +0800

    todo-使用canvas绘制

--- src/js/repeatCube/index.js
+++ src/js/repeatCube/index.js
@@ -3,6 +3,8 @@ import {
   MeshLambertMaterial,
   Mesh,
   TextureLoader,
+  CubeReflectionMapping,
+  CubeRefractionMapping,
 } from 'three';
 import mergeImg from '@/assets/image/merge.png';
 
@@ -13,24 +15,46 @@ class CubeRender {
     const { scene } = val;
     this.scene = scene;
 
-    this.mergeTex = textureLoader.load(mergeImg);
-    this.mergeTex.repeat.set(3, 2);
-    this.mergeTex.offset.set(-0.8, -0.6);
-
+    // this.mergeTex = textureLoader.load(mergeImg);
     this.init();
   }
 
   init() {
     // 添加立方体
-    const geometry = new BoxGeometry(40, 40, 40);
-    const material = new MeshLambertMaterial({
-      // color: 0x334455,
-      map: this.mergeTex,
-    });
+    // const geometry = new BoxGeometry(40, 40, 40);
+
+    // 生成纹理
+    const image = new Image();
+    image.src = mergeImg;
+    image.onload = (e) => {
+      this.generateMaterial(e, image);
+    }
+
+    // const material = new MeshLambertMaterial({
+    //   // color: 0x334455,
+    //   map: this.mergeTex,
+    // });
     
-    const mesh = new Mesh(geometry, material);
-    mesh.position.set(0, 0, 0);
-    this.scene.add(mesh);
+    // const mesh = new Mesh(geometry, material);
+    // mesh.position.set(0, 0, 0);
+    // this.scene.add(mesh);
+  }
+
+  generateMaterial(e, image) {
+    const { width, height } = e.path[0];
+    const singleWidth = width / 3;
+    const singleHeight = height / 2;
+    const canvasArr = [];
+
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 3; j++) {
+        const newCanvas = document.createElement('canvas');
+        const ctx = newCanvas.getContext('2d');
+        // todo ---------------
+        // 上 - 中
+        ctx.drawImage(image, );
+      }
+    }
   }
 }
 
