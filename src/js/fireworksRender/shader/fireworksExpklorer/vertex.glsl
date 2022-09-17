1c2e4f80479714db3df8b078dc4728def8f4b710
wind-hao-laptop <windapes@163.com>
2022-09-17 08:38:07 +0800

    删除多余文件

--- src/js/fireworksRender/shader/fireworksExpklorer/vertex.glsl
+++
@@ -1,7 +0,0 @@
-
-void main() {
-  vec4 sourcePosition = vec4(sourcePosition, 1.0);
-  vec4 modelPosition = modelMatrix * sourcePosition;
-  vec4 viewPosition = viewMatrix * modelPosition;
-  gl_Posiiton = projectionMatrix * viewPosition;
-}
