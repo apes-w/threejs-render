32956aff895d9943a06a4a3e5ebce990f4c46cb6
wind-hao-laptop <windapes@163.com>
2022-10-25 14:06:18 +0800

    实现在点击位置的卡片渲染纹理

--- src/js/decorationDesignRender/shader/fragmentShader.glsl
+++ src/js/decorationDesignRender/shader/fragmentShader.glsl
@@ -15,25 +15,6 @@ varying vec3 vNormal;
 varying vec3 vPosition;
 varying vec3 vNormalPosition;
 
-// 获取一个方形的区域
-bool getCardSize(const in vec3 geoPosition, const in vec3 geoNormal) {
-  if (abs(geoNormal.y) > 0.0) return false;
-
-  vec3 geoVec = geoPosition - uClickPoint;
-  float geoLength = length(geoVec);
-
-  vec3 xAxesVec = normalize(vec3(-1.0 / geoNormal.x, 0.0, 1.0 / geoNormal.z));
-  vec3 yAxesVec = vec3(0.0, 1.0, 0.0);
-
-  float angleCos = dot(normalize(geoVec), xAxesVec);
-  float angleSin = sqrt(1.0 - pow(angleCos, 2.0));
-
-  if (
-    abs(geoLength * angleCos) <= uCardSize / 2.0
-    && abs(geoLength * angleSin) <= uCardSize / 2.0
-  ) return true;
-  return false;
-}
 
 void main() {
 
@@ -45,15 +26,43 @@ void main() {
   // 计算反射后的颜色，光线颜色 * 物体颜色
   vec3 resLightColor = uLightColor * realColor * dotL;
   vec3 resAmbientColor = realColor * uAmbientLightColor;
+
+  vec3 geoVec = vPosition - uClickPoint;
+  float geoVecLength = length(geoVec);
+
+  vec3 xAxesVec = normalize(vec3(-1.0 / vNormal.x, 0.0, 1.0 / vNormal.z));
+  float angleCos = dot(normalize(geoVec), xAxesVec);
+  float angleSin = sqrt(1.0 - pow(angleCos, 2.0));
+  float geoVecXLength = abs(geoVecLength * angleCos);
+  float geoVecYLength = abs(geoVecLength * angleSin);
+
   // 检测当前的法向量和传入的点击位置的法向量是否平行
   if (
     // 已经传入了点击位置的面的法向量
     (abs(uClickNormal.x) + abs(uClickNormal.y) + abs(uClickNormal.z) > 0.0)
     && (dot(normalize(vNormal), uClickNormal) >= 0.9945218953)
     // 在点击位置制作一个cardSize大小的卡片渲染区域
-    && getCardSize(vPosition, vNormal)
+    && vNormal.y <= 0.0
+    && (
+      geoVecXLength <= uCardSize / 2.0
+      && geoVecYLength <= uCardSize / 2.0
+    )
   ) {
-    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    float xUV = 0.0, yUV = 0.0;
+    if (geoVec.x < 0.0) {
+      xUV = uCardSize / 2.0 - geoVecXLength;
+    } else {
+      xUV = uCardSize / 2.0 + geoVecXLength;
+    }
+    if (geoVec.y < 0.0) {
+      yUV = uCardSize / 2.0 - geoVecYLength;
+    } else {
+      yUV = uCardSize / 2.0 + geoVecYLength;
+    }
+    vec2 colorUV = vec2(xUV / uCardSize, yUV / uCardSize);
+    gl_FragColor = texture2D(uTexture, colorUV);
+    // gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    // gl_FragColor = vec4(geoVec, 1.0);
     // gl_FragColor = vec4(vec3(1.0, 0.0, 1.0) * (resLightColor + resAmbientColor), 1.0);
   } else {
     gl_FragColor = vec4(realColor * (resLightColor + resAmbientColor), 1.0);
